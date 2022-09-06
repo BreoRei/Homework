0fcd7125df10dca665d8d4fe6455b0fdb182de5e
Artem <artemii0307@gmail.com>
2022-09-07 00:33:19 +0500

    add Seminar_3 Work_21

--- Seminar_3/Work_21/Program.cs
+++ Seminar_3/Work_21/Program.cs
@@ -14,4 +14,5 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 int z2 = Convert.ToInt32(Console.ReadLine());
 
 double AB = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine(AB);
+Console.Clear();
+Console.WriteLine($"Расстояние между точками в 3D пространстве: \n{AB}");
