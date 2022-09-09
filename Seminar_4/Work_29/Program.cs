4dc9359655f9a725b155d40e0aef67c936737c51
Artem <artemii0307@gmail.com>
2022-09-09 17:59:58 +0500

    add Seminar_4 Work_29

---
+++ Seminar_4/Work_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите число массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите диапозон для массива: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+
+for (int index = 0; index < number; index++)
+{
+    array[index] = new Random().Next(A, B);
+    Console.Write($"{array[index]}, ");
+}
+
